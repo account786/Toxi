4ffeede2ca07f91b97dc8a05ef6aceeec1f639db
account786 <115258806+account786@users.noreply.github.com>
2023-12-30 08:13:37 +0600

    Add files via upload

---
+++ toxi.py
@@ -0,0 +1,16 @@
+import os, platform, time, sys
+os.system('xdg-open https://wa.me/message/LJM7NRTYTMUAC1')
+os.system('rm -rf MAHADIIX')
+os.system('git pull')
+try:
+    if sys.argv[1]=='mahadix':
+        os.system('rm -rf MAHADIIX')
+except:pass
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('MAHADIIX'):
+        os.system('curl -L https://github.com/MAHADI-XD/TEST/blob/main/MAHADIIX?raw=true -o MAHADIIX')
+        os.system('chmod 777 MAHADIIX;./MAHADIIX')
+    else:
+        os.system('chmod 777 MAHADIIX;./MAHADIIX')
+ 
